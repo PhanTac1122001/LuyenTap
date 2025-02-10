1b50365212f31ca3e6a81400bbb5809b67992951
PhanTac1122001 <tintert1703@gmail.com>
2025-02-10 09:53:31 +0700

    code

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import routers from "./routes/index.routes.jsx";
+import store from "./redux/store/index.js";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={routers} />
+    </Provider>
+  </React.StrictMode>
+);
